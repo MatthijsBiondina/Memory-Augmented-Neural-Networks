4aefe6d0f7ee7f7c376b93a33c1ba814dd265a71
MatthijsBiondina <biondina.matthijs@gmail.com>
2020-10-15 18:24:11 +0200

    dnc

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import subprocess
 while True:
     lr = eval(f"1e-{random.uniform(2, 6)}")
     bs = int(eval(f"2**{random.uniform(4, 8)}"))
-    ms = 32
+    ms = 33
 
     subprocess.run([f"python main.py {lr} {bs} {ms}"], shell=True, check=True)
