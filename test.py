29dab59454696ed0c6650fc10e4931d21cb8ed2a
MatthijsBiondina <biondina.matthijs@gmail.com>
2020-10-15 18:08:17 +0200

    dnc

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import subprocess
 while True:
     lr = eval(f"1e-{random.randint(2, 6)}")
     bs = random.randint(8, 512)
-    ms = 32
+    ms = random.randint(20,64)
 
     subprocess.run([f"python main.py {lr} {bs} {ms}"], shell=True, check=True)
