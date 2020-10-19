35b0b862ad80d55c5a3d504e6aa603871d92d2fd
MatthijsBiondina <biondina.matthijs@gmail.com>
2020-10-19 18:03:32 +0200

    visualization and auto tmux

--- tmux.py
+++ tmux.py
@@ -4,6 +4,7 @@ import subprocess
 
 def start_session(ii, cuda=''):
     cmd = f"tmux new-session -d -s {ii}_hypersearch_{ii} 'source ~/venv/bin/activate && cd ~/MANN && CUDA_VISIBLE_DEVICES={cuda} python test.py'"
+    print(cmd)
     subprocess.run([cmd])
 
 
