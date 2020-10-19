7aea3dc29f9dfda9eaa7be2d2a88ed29c40ae01a
MatthijsBiondina <biondina.matthijs@gmail.com>
2020-10-19 18:07:15 +0200

    visualization and auto tmux

--- tmux.py
+++ tmux.py
@@ -3,9 +3,10 @@ import subprocess
 
 
 def start_session(ii, cuda=''):
-    cmd = f"tmux new-session -d -s {ii}_hypersearch_{ii} 'source ~/venv/bin/activate && cd ~/MANN && CUDA_VISIBLE_DEVICES={cuda} python test.py'"
+    # cmd = f"tmux new-session -d -s {ii}_hypersearch_{ii} 'source ~/venv/bin/activate && cd ~/MANN && CUDA_VISIBLE_DEVICES={cuda} python test.py'"
+    cmd = f"tmux new-session -d -s {ii}_hypersearch_{ii} 'source ~/venv/bin/activate'"
     print(cmd)
-    subprocess.run([cmd])
+    subprocess.run([cmd], shell=True, check=True)
 
 
 for ii in range(5):
